780edfe9107a08f16864c84065492abf28a8e47c
ZaneS5 <zane_sauer@yahoo.com>
2023-02-11 23:19:03 -0700

    random changes from testing

---
+++ test.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+
+LOCAL_HOST = "127.0.0.1"
+BROKER_PORT = 9001
+KEEP_ALIVE = 60
+
+topic = "$SYS/#"
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(topic)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(LOCAL_HOST, BROKER_PORT, KEEP_ALIVE)
+client.publish("test", "bing bong", 0, True)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
